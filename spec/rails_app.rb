0ef69a88a2991a39aab1ddd25fd311dae4f86d68
hck <alexsvirin@gmail.com>
2016-04-20 22:30:05 +0200

    Update RSpec to v3.4.

--- spec/rails_app.rb
+++ spec/rails_app.rb
@@ -6,4 +6,5 @@ app = Class.new(Rails::Application)
 app.config.secret_token = Digest::MD5.hexdigest("mongoid_will_paginate")
 app.config.session_store :cookie_store, :key => "_test_rails_app_session"
 app.config.active_support.deprecation = :log
+app.config.eager_load = false
 app.initialize!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,43 @@
 require 'rubygems'
-require 'bundler/setup'
 
 require 'rails'
 require 'mongoid'
 require 'will_paginate'
 require 'mongoid_will_paginate'
 
-require File.join(File.dirname(__FILE__), 'rails_app')
+require 'rails_app'
 require 'rspec/rails'
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
-RSpec.configure do |config|
-  config.mock_with :rspec
-  config.before :each do
-    Mongoid.master.collections.select{|c| c.name !~ /system/}.each(&:drop)
-  end
+Mongoid.configure do |config|
+  config.connect_to 'test_mongoid_will_paginate'
 end
 
-Mongoid.configure{|c| c.master = Mongo::Connection.new.db("test_mongoid_will_paginate")}
+Mongoid.logger.level = Logger::ERROR
+Mongo::Logger.logger.level = Logger::ERROR
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  config.example_status_persistence_file_path = 'spec/examples.txt'
+
+  config.disable_monkey_patching!
+
+  config.profile_examples = 10
+
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.before(:each) do
+    Mongoid.default_client.collections.select{ |c| c.name !~ /system/ }.each(&:drop)
+  end
+end
