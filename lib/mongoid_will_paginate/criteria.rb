63f5bbc6e309ef53e1098b4a09fe677eef9ab4bf
hck <alexsvirin@gmail.com>
2016-04-25 21:58:08 +0200

    Remove redundant self.

--- lib/mongoid_will_paginate/criteria.rb
+++ lib/mongoid_will_paginate/criteria.rb
@@ -17,10 +17,11 @@ module MongoidWillPaginate
       def paginate(options = {})
         page = options[:page] || 1
         per_page = options[:per_page] || WillPaginate.per_page
-        total = options[:total_entries] || self.count
+        total = options[:total_entries] || count
 
         WillPaginate::Collection.create(page, per_page, total) do |pager|
-          pager.replace self.skip(pager.offset).limit(pager.per_page).to_a
+          col = skip(pager.offset).limit(pager.per_page).to_a
+          pager.replace(col)
         end
       end
     end
